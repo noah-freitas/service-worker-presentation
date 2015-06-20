42553bf27b1463916b466ebabf5bfecd492bd836
Noah Freitas <noah.j.freitas@gmail.com>
2015-06-19 19:14:59 -0700

    Add console.log service worker demo.

---
+++ demo/sw.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var version = 1;
+
+self.addEventListener('install'  , handleInstall);
+self.addEventListener('activate' , handleActivate);
+self.addEventListener('fetch'    , handleFetch);
+
+// handleInstall :: InstallEvent -> undefined
+function handleInstall(e) {
+    console.log('[INSTALL] event:', e);
+}
+
+// handleActivate :: ExtendableEvent -> undefined
+function handleActivate(e) {
+    console.log('[ACTIVATE] event:', e);
+}
+
+// handleFetch :: FetchEvent -> undefined
+function handleFetch(e) {
+    console.log('[FETCH] event:', e);
+
+    if (isVersionRequest(e.request)) return e.respondWith(versionResponse());
+    e.respondWith(fetch(e.request).then(logAndReturnResponse));
+
+    // isVersionRequest :: Request -> Boolean
+    function isVersionRequest(req) {
+        var url = new URL(req.url);
+
+        return req.method   === 'GET'
+            && url.pathname === '/version';
+    }
+
+    // logAndReturnResponse :: Response -> Response
+    function logAndReturnResponse(resp) {
+        console.log('[RESPONSE] response:', resp);
+        return resp;
+    }
+
+    // versionResponse :: undefined -> Response
+    function versionResponse() {
+        console.log('Reporting version', version);
+        return new Response(JSON.stringify({ version : version }), {
+            headers : new Headers({ 'Content-Type' : 'application/json' })
+        });
+    }
+}
